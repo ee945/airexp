38a7b95ae441308623da800ca6d98f089b5bff0b
ee945 <ee945@126.com>
2016-09-23 16:46:22 +0800

    modify: routes.php - add some route temp.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hawb', 'HawbController@list');
+Route::get('hawb/show', 'HawbController@show');
+Route::get('hawb/print', 'HawbController@print');
+Route::post('hawb/add', 'HawbController@add');
+Route::post('hawb/edit', 'HawbController@edit');
+Route::post('hawb/del', 'HawbController@del');
+
+Route::get('mawb', 'MawbController@list');
+Route::get('mawb/show', 'MawbController@show');
+Route::get('mawb/print', 'MawbController@print');
+Route::post('mawb/add', 'MawbController@add');
+Route::post('mawb/edit', 'MawbController@edit');
+Route::post('mawb/del', 'MawbController@del');
+
+Route::get('manifest', 'ManifestController@search');
+Route::get('manifest/show', 'ManifestController@show');
+Route::get('manifest/print', 'ManifestController@print');
+
+Route::get('report/daily', 'ReportController@daily');
+Route::get('report/month', 'ReportController@month');
+Route::get('report/forward', 'ReportController@forward');
+
+Route::get('client', 'ClientController@forward');
+Route::get('address', 'AddressController@forward');
+Route::get('port', 'PortController@forward');
+
+Route::get('setting', 'ReportController@forward');
