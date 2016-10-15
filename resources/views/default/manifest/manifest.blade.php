4b90dc129aeafb23764e20d972d330bebdb59db6
ee945 <ee945@126.com>
2016-10-16 00:45:58 +0800

    modify: manifest.blade - optimize style

--- resources/views/default/manifest/manifest.blade.php
+++ resources/views/default/manifest/manifest.blade.php
@@ -7,55 +7,59 @@
         <div style="width:960px;">
         <p style="margin-left:35%;margin-right:65%;margin-bottom:15px;width:500px;font-weight:bold;font-size:26px;">HOUSE CARGO MANIFEST</p>
         </div>
-        <table border="1" cellpadding="0" cellspacing="0" style="width:960px;border-collapse:collapse;font-size:12px">
+        <style>
+          .mani {width:960px;font-size:12px;}
+          .mani tr td{font-family:宋体;padding:3px 5px;}
+        </style>
+        <table class="mani" border="1">
           <tr>
-            <td width="25%" align="center" style="padding:3px 5px;">Air Freight Agent</td>
-            <td width="15%" align="center" style="padding:3px 5px;">Master AWB No.</td>
-            <td width="15%" align="center" style="padding:3px 5px;">Port of Discharge</td>
-            <td width="15%" align="center" style="padding:3px 5px;">Total No.of Shipment</td>
-            <td width="15%" align="center" style="padding:3px 5px;">Flight No.</td>
-            <td width="15%" align="center" style="padding:3px 5px;">Date</td>
+            <td width="25%" align="center">Air Freight Agent</td>
+            <td width="15%" align="center">Master AWB No.</td>
+            <td width="15%" align="center">Port of Discharge</td>
+            <td width="15%" align="center">Total No.of Shipment</td>
+            <td width="15%" align="center">Flight No.</td>
+            <td width="15%" align="center">Date</td>
           </tr>
           <tr>
-            <td style="padding:3px 5px;"></td>
-            <td style="padding:3px 5px;">{{ $hawb_first->mawb }}</span></td>
-            <td style="padding:3px 5px;">{{ $hawb_first->dest }}</span></td>
-            <td style="padding:3px 5px;">{{ $hawb_count }}</span></td>
-            <td style="padding:3px 5px;">{{ $hawb_first->fltno }}</td>
-            <td style="padding:3px 5px;">{{ $hawb_first->fltdate }}</td>
+            <td></td>
+            <td>{{ $hawb_first->mawb }}</span></td>
+            <td>{{ $hawb_first->dest }}</span></td>
+            <td>{{ $hawb_count }}</span></td>
+            <td>{{ $hawb_first->fltno }}</td>
+            <td>{{ $hawb_first->fltdate }}</td>
           </tr>
         </table>
         <br />
-        <table border="1" cellspacing="0" cellpadding="0" style="width:960px;border-collapse:collapse;font-size:12px">
+        <table class="mani" border="1">
           <tr>
-            <td width="96px" style="padding:3px 5px;"> Hawb No.</td>
-            <td width="48px" style="padding:3px 5px;"> No. of Pkg</td>
-            <td width="50px" style="padding:3px 5px;"> WT. in Kilo(Lb)</td>
-            <td width="187px" style="padding:3px 5px;"> Nature of Goods</td>
-            <td width="60px" style="padding:3px 5px;"> Port of Lading</td>
-            <td width="60px" style="padding:3px 5px;"> Final Dest</td>
-            <td width="177px" style="padding:3px 5px;">Name &amp; Address of Shipper</td>
-            <td width="177px" style="padding:3px 5px;">Name &amp; Address of Consignee</td>
-            <td width="106px" style="padding:3px 5px;">For Offical Use Only</td>
+            <td width="96px"> Hawb No.</td>
+            <td width="48px"> No. of Pkg</td>
+            <td width="50px"> WT. in Kilo(Lb)</td>
+            <td width="187px"> Nature of Goods</td>
+            <td width="60px"> Port of Lading</td>
+            <td width="60px"> Final Dest</td>
+            <td width="177px">Name &amp; Address of Shipper</td>
+            <td width="177px">Name &amp; Address of Consignee</td>
+            <td width="106px">For Offical Use Only</td>
           </tr>
           @foreach($hawbs as $hawb)
           <tr>
-            <td style="padding:3px 5px;">{{ "YH-".$hawb->hawb }}</td>
-            <td style="padding:3px 5px;">{{ $hawb->num }}</td>
-            <td style="padding:3px 5px;">{{ $hawb->gw }}</td>
-            <td style="padding:3px 5px;">{{ substr($hawb->cgodescp,0,stripos($hawb->cgodescp, "\n")) }}</td>
-            <td style="padding:3px 5px;">SHA</td>
-            <td style="padding:3px 5px;">{{ $hawb->dest }}</td>
-            <td style="padding:3px 5px;">{{ str_replace('\n',"\n",$hawb->shipper) }}</td>
-            <td style="padding:3px 5px;">{{ str_replace('\n',"\n",$hawb->consignee) }}</td>
-            <td style="padding:3px 5px;"></td>
+            <td>{{ "YH-".$hawb->hawb }}</td>
+            <td>{{ $hawb->num }}</td>
+            <td>{{ $hawb->gw }}</td>
+            <td>{{ substr($hawb->cgodescp,0,stripos($hawb->cgodescp, "\n")) }}</td>
+            <td>SHA</td>
+            <td>{{ $hawb->dest }}</td>
+            <td>{{ str_replace('\n',"\n",$hawb->shipper) }}</td>
+            <td>{{ str_replace('\n',"\n",$hawb->consignee) }}</td>
+            <td></td>
           </tr>
           @endforeach
           <tr>
-            <td style="padding:3px 5px;">TOTAL:</td>
-            <td style="padding:3px 5px;">{{$amount_num}}</td>
-            <td style="padding:3px 5px;">{{$amount_gw}}</td>
-            <td style="padding:3px 5px;" colspan=6></td>
+            <td>TOTAL:</td>
+            <td>{{$amount_num}}</td>
+            <td>{{$amount_gw}}</td>
+            <td colspan=6></td>
           </tr>
         </table>
         <div class="text-center" style="width:880px;padding:10px 0px;">
