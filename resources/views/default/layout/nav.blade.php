1d9760b6de5caf5fe32b151af1a7e6e86cc426f1
ee945 <ee945@126.com>
2017-01-13 00:32:10 +0800

    adjust nav

--- resources/views/default/layout/nav.blade.php
+++ resources/views/default/layout/nav.blade.php
@@ -13,22 +13,12 @@
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="{{ url('/') }}">首页</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">进仓编号<span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ route('jincang_add') }}">进仓登记</a></li>
-                <li><a href="{{ route('jincang_list') }}">进仓列表</a></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">分单操作<span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ route('hawb_add') }}">分单输入</a></li>
-                <li><a href="{{ route('hawb_list') }}">分单列表</a></li>
-              </ul>
-            </li>
+            <li><a href="{{ route('jincang_add') }}">进仓登记</a></li>
+            <li><a href="{{ route('jincang_list') }}">进仓列表</a></li>
+            <li><a href="{{ route('hawb_add') }}">分单输入</a></li>
+            <li><a href="{{ route('hawb_list') }}">分单列表</a></li>
             <li><a href="{{ route('mawb_list') }}">总单列表</a></li>
-            <li class="dropdown">
+            {{-- <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">统计报表<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="#">货量统计</a></li>
@@ -36,7 +26,7 @@
                 <li><a href="#">每月货量</a></li>
                 <li><a href="#">销售分析</a></li>
               </ul>
-            </li>
+            </li> --}}
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">数据维护<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
