c9b0f4e96876da4f164301ce57a5458a8038012c
Ubuntu <ubuntu@ip-172-31-27-4.ap-northeast-2.compute.internal>
2017-01-16 22:05:12 +0800

    adjust track view: input search on mobile device to one line

--- resources/views/default/track/track.blade.php
+++ resources/views/default/track/track.blade.php
@@ -8,7 +8,7 @@
           <thead>
             <tr>
               <th colspan="8" style="font-size: 1.3em;">
-                <input type="text" id="search_mawb" placeholder="查询总单">
+                <input type="text" id="search_mawb" placeholder="查询总单" style="width:50%">
                 <a id="search_arrival" type="button" target="_blank" class="btn btn-success" style="width:80px;margin-left: 10px;">运抵</a>
                 <a id="search_flight" type="button" target="_blank" class="btn btn-primary" style="width:80px;margin-left: 10px;">航班</a>
               </th>
